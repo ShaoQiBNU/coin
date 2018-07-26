334bdacdb89915ee3d45bebafbf8bd4a5c3b87ca
ShaoQiBNU <37075909+ShaoQiBNU@users.noreply.github.com>
2018-07-26 15:45:15 +0800

    Add files via upload

---
+++ coins3.py
@@ -0,0 +1,16 @@
+def coin_element(n,coins):
+
+    dp=[0]*(n+1)
+
+    for i in range(1,n+1):
+        dp[i]=i
+        for j in coins:
+            dp[i]=min(dp[i],dp[i-j])+1
+    return dp[n]
+
+
+if __name__ == '__main__':
+    n=11
+    coins=[1,3,5]
+    res=coin_element(n,coins)
+    print(res)
