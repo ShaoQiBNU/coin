88ac675aec90913cd2626204cc51ef577bb1bd8e
ShaoQiBNU <37075909+ShaoQiBNU@users.noreply.github.com>
2019-07-31 21:18:39 +0800

    Update coins3.py

--- coins3.py
+++ coins3.py
@@ -5,7 +5,8 @@ def coin_element(n,coins):
     for i in range(1,n+1):
         dp[i]=i
         for j in coins:
-            dp[i]=min(dp[i],dp[i-j])+1
+            if i-j>=0:
+                dp[i]=min(dp[i],dp[i-j])+1
     return dp[n]
 
 
